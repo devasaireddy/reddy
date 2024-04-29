a556d36b7c92eb6a219570b19aa8daede3387e8b
devasaireddy <157039438+devasaireddy@users.noreply.github.com>
2024-04-29 23:39:15 +0530

    Create sai.py

---
+++ sai.py
@@ -0,0 +1,16 @@
+git clone link
+cd repo name
+#any changes in code
+git add .
+git commit -m "add new line"
+git push origin main
+git pull origin main
+#create branch
+git checkout -b new branch name
+#goto main branch
+git checkout main
+git merge new branch name
+#issues
+git add .
+git commit -m "Fix #1: dfggh"
+git push origin main
